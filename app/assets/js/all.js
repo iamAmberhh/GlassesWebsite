59a452e3a3f8df0c973d22b9e393c80219b2c220
Amber <melody19941231@gmail.com>
2022-07-24 13:29:37 +0800

    Update 2022-07-24T05:29:29.338Z

--- app/assets/js/all.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(() => {
-  $('.mobile-show').click(function (e) { 
-    e.preventDefault();
-    $('.header-menu').toggleClass('show');
-  });
-});
---
+++ assets/js/all.js
@@ -0,0 +1,9 @@
+"use strict";
+
+$(document).ready(function () {
+  $('.mobile-show').click(function (e) {
+    e.preventDefault();
+    $('.header-menu').toggleClass('show');
+  });
+});
+//# sourceMappingURL=all.js.map
